008b6cd4c1da722556ef6d329d6d1b20c4f4a15a
Lucas França <102264849+Suubiprabaxo@users.noreply.github.com>
2022-09-19 13:25:25 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'test';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+    
+?>
